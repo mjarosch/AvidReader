43941e7562c35941df37698a8cab6e0d38bc367f
Mike <mike@mikejarosch.com>
2020-12-28 11:56:42 -0600

    Initial commit

---
+++ AvidReader/AvidReaderApp.swift
@@ -0,0 +1,17 @@
+//
+//  AvidReaderApp.swift
+//  AvidReader
+//
+//  Created by Mike Jarosch on 12/28/20.
+//
+
+import SwiftUI
+
+@main
+struct AvidReaderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
